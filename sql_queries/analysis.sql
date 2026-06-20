62eb6aa71d6e47bdef1cbf4245e93edaacf8ff16
Ayan Goswami <ayan.goswami11799@gmail.com>
2026-06-20 16:51:08 +0530

    Add SQL queries for clinical trial data analysis

--- sql_queries/analysis.sql
+++ sql_queries/analysis.sql
@@ -1,1 +1,21 @@
 -- Clinical Trial Data Analysis Queries
+-- Clinical Trial Data Analysis
+
+SELECT *
+FROM clinical_trials;
+
+SELECT COUNT(*)
+FROM clinical_trials;
+
+SELECT AVG(age)
+FROM clinical_trials;
+
+SELECT treatment_group,
+       COUNT(*) AS patients
+FROM clinical_trials
+GROUP BY treatment_group;
+
+SELECT outcome,
+       COUNT(*) AS total
+FROM clinical_trials
+GROUP BY outcome;
