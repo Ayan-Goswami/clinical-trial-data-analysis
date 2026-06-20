8f56a749155a5b39a8f8704c02015ea44b95652c
Ayan Goswami <ayan.goswami11799@gmail.com>
2026-06-20 20:42:47 +0530

    Update analysis.sql

--- sql_queries/analysis.sql
+++ sql_queries/analysis.sql
@@ -35,3 +35,45 @@ SUM(stroke) AS stroke_cases
 FROM stroke_data
 GROUP BY hypertension;
 
+-- Which gender has the higher stroke rate?
+SELECT
+    gender,
+    SUM(stroke) AS stroke_cases,
+    COUNT(*) AS total_patients,
+    ROUND((SUM(stroke)::numeric / COUNT(*))*100,2) AS stroke_rate
+FROM stroke_data
+GROUP BY gender;
+
+-- Which smoking category has the highest stroke rate?
+SELECT
+    smoking_status,
+    COUNT(*) AS total_patients,
+    SUM(stroke) AS stroke_cases,
+    ROUND((SUM(stroke)::numeric / COUNT(*))*100,2) AS stroke_rate
+FROM stroke_data
+GROUP BY smoking_status
+ORDER BY stroke_rate DESC;
+
+-- whether former smokers are older on average.
+SELECT
+    smoking_status,
+    ROUND(AVG(age),2) AS average_age
+FROM stroke_data
+GROUP BY smoking_status
+ORDER BY average_age DESC;
+
+--"How does stroke risk change across age groups?"
+SELECT
+    CASE
+        WHEN age < 30 THEN 'Under 30'
+        WHEN age BETWEEN 30 AND 49 THEN '30-49'
+        WHEN age BETWEEN 50 AND 69 THEN '50-69'
+        ELSE '70+'
+    END AS age_group,
+    COUNT(*) AS total_patients,
+    SUM(stroke) AS stroke_cases,
+    ROUND((SUM(stroke)::numeric / COUNT(*))*100,2) AS stroke_rate
+FROM stroke_data
+GROUP BY age_group
+ORDER BY stroke_rate DESC;
+
